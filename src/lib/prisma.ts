e159e96dde8cf104e50a70e19367558dfa5efd37
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-22 15:22:54 +0700

    Configure Prisma and Neon db

---
+++ src/lib/prisma.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+	return new PrismaClient()
+}
+
+declare global {
+	var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
