ada979e7999baa2ee38434e0c9dfacc5d6fdfc50
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-22 14:35:50 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import React from 'react'
+
+import './globals.css'
+
+const geistSans = localFont({
+	src: './fonts/GeistVF.woff',
+	variable: '--font-geist-sans',
+})
+
+const geistMono = localFont({
+	src: './fonts/GeistMonoVF.woff',
+	variable: '--font-geist-mono',
+})
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | bugbook',
+		default: 'bugbook',
+	},
+	description: 'The Social Media App for Powernerds',
+}
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang='en'>
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				{children}
+			</body>
+		</html>
+	)
+}
