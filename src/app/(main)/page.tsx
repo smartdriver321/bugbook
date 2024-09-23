ddd12b2fde8795c9ce1b2c32f551587c7eee48eb
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-23 19:08:04 +0700

    Loading post  from server-side (server component)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,21 @@
+import prisma from '@/lib/prisma'
+import { postDataInclude } from '@/types/types'
 import PostEditor from '@/components/posts/editor/PostEditor'
+import Post from '@/components/posts/Post'
 
-export default function HomePage() {
+export default async function HomePage() {
+	const posts = await prisma.post.findMany({
+		include: postDataInclude,
+		orderBy: { createdAt: 'desc' },
+	})
+
 	return (
-		<main className='flex w-full min-w-0 gap-5'>
+		<main className='w-full min-w-0'>
 			<div className='w-full min-w-0 space-y-5'>
 				<PostEditor />
+				{posts.map((post) => (
+					<Post key={post.id} post={post} />
+				))}
 			</div>
 		</main>
 	)
