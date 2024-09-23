05e11ce6bd6e03cf6fe164b72cfe2edff76768b1
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-23 11:12:22 +0700

    Create LoginPage and its functionality

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,37 @@
-export default function LoginPage() {
-	return <div>LoginPage</div>
+import loginImage from '../../../../public/assets/login-image.jpg'
+import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import LoginForm from './LoginForm'
+
+export const metadata: Metadata = {
+	title: 'Login',
+}
+
+export default function LogInPage() {
+	return (
+		<main className='flex h-screen items-center justify-center p-5'>
+			<div className='flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl'>
+				<div className='w-full space-y-10 overflow-y-auto p-10 md:w-1/2'>
+					<h1 className='text-center text-3xl font-bold'>Login to BugBook</h1>
+					<div className='space-y-5'>
+						<div className='flex items-center gap-3'>
+							<div className='h-px flex-1 bg-muted' />
+							<span>OR</span>
+							<div className='h-px flex-1 bg-muted' />
+						</div>
+						<LoginForm />
+						<Link href='/signup' className='block text-center hover:underline'>
+							Don&apos;t have an account? Register
+						</Link>
+					</div>
+				</div>
+				<Image
+					src={loginImage}
+					alt=''
+					className='hidden w-1/2 object-cover md:block'
+				/>
+			</div>
+		</main>
+	)
 }
