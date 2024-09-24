3f077255c8762fa8013ad303bcc958879e4164bf
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-24 13:00:51 +0700

    useInfiniteQuery (infinite loading, cursor-based pagination, react-intersection-observer)

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -1,23 +1,50 @@
 'use client'
 
 import { Loader2 } from 'lucide-react'
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 
-import { PostData } from '@/types/types'
-import Post from '@/components/posts/Post'
+import { PostsPage } from '@/types/types'
 import kyInstance from '@/lib/ky'
+import PostsLoadingSkeleton from '@/components/posts/PostsLoadingSkeleton'
+import InfiniteScrollContainer from '@/components/InfiniteScrollContainer'
+import Post from '@/components/posts/Post'
 
 export default function ForYouFeed() {
-	const query = useQuery<PostData[]>({
+	const {
+		data,
+		fetchNextPage,
+		hasNextPage,
+		isFetching,
+		isFetchingNextPage,
+		status,
+	} = useInfiniteQuery({
 		queryKey: ['post-feed', 'for-you'],
-		queryFn: kyInstance.get('/api/posts/for-you').json<PostData[]>,
+		queryFn: ({ pageParam }) =>
+			kyInstance
+				.get(
+					'/api/posts/for-you',
+					pageParam ? { searchParams: { cursor: pageParam } } : {}
+				)
+				.json<PostsPage>(),
+		initialPageParam: null as string | null,
+		getNextPageParam: (lastPage) => lastPage.nextCursor,
 	})
 
-	if (query.status === 'pending') {
-		return <Loader2 className='mx-auto animate-spin' />
+	const posts = data?.pages.flatMap((page) => page.posts) || []
+
+	if (status === 'pending') {
+		return <PostsLoadingSkeleton />
 	}
 
-	if (query.status === 'error') {
+	if (status === 'success' && !posts.length && !hasNextPage) {
+		return (
+			<p className='text-center text-muted-foreground'>
+				No one has posted anything yet.
+			</p>
+		)
+	}
+
+	if (status === 'error') {
 		return (
 			<p className='text-center text-destructive'>
 				An error occurred while loading posts.
@@ -26,10 +53,14 @@ export default function ForYouFeed() {
 	}
 
 	return (
-		<div className='space-y-5'>
-			{query.data.map((post) => (
+		<InfiniteScrollContainer
+			className='space-y-5'
+			onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+		>
+			{posts.map((post) => (
 				<Post key={post.id} post={post} />
 			))}
-		</div>
+			{isFetchingNextPage && <Loader2 className='mx-auto my-3 animate-spin' />}
+		</InfiniteScrollContainer>
 	)
 }
--- src/app/api/posts/for-you/route.ts
+++ src/app/api/posts/for-you/route.ts
@@ -1,10 +1,13 @@
 import { validateRequest } from '@/auth'
 import prisma from '@/lib/prisma'
-import { postDataInclude } from '@/types/types'
+import { postDataInclude, PostsPage } from '@/types/types'
 
 export async function GET() {
 	try {
 		const { user } = await validateRequest()
+		const cursor = req.nextUrl.searchParams.get('cursor') || undefined
+
+		const pageSize = 10
 
 		if (!user) {
 			return Response.json({ error: 'Unauthorized' }, { status: 401 })
@@ -13,9 +16,18 @@ export async function GET() {
 		const posts = await prisma.post.findMany({
 			include: postDataInclude,
 			orderBy: { createdAt: 'desc' },
+			take: pageSize + 1,
+			cursor: cursor ? { id: cursor } : undefined,
 		})
 
-		return Response.json(posts)
+		const nextCursor = posts.length > pageSize ? posts[pageSize].id : null
+
+		const data: PostsPage = {
+			posts: posts.slice(0, pageSize),
+			nextCursor,
+		}
+
+		return Response.json(data)
 	} catch (error) {
 		console.error(error)
 		return Response.json({ error: 'Internal server error' }, { status: 500 })
